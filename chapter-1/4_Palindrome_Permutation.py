2b870ace40e31b64b8b4d0ab17b0996afd143981
James Cockbain <james.cockbain@ibm.com>
2019-08-18 19:13:07 +0100

    added solution for 1.4

---
+++ chapter-1/4_Palindrome_Permutation.py
@@ -0,0 +1,28 @@
+def pal_perm(phrase):
+    '''function checks if a string is a permutation of a palindrome or not'''
+    table = [0 for _ in range(ord('z') - ord('a') + 1)]
+    print(table)
+    countodd = 0
+    for c in phrase:
+        x = char_number(c)
+        if x != -1:
+            table[x] += 1
+            if table[x] % 2:
+                countodd += 1
+            else:
+                countodd -= 1
+    return countodd <= 1
+
+
+def char_number(c):
+    a = ord('a')
+    z = ord('z')
+    A = ord('A')
+    Z = ord('Z')
+    val = ord(c)
+
+    if a <= val <= z:
+        return val - a
+    elif A <= val <= Z:
+        return val - A
+    return -1
