ed493b17ae7624e19c0644b9c0e61b6ae3604b6f
James Cockbain <james.cockbain@ibm.com>
2019-08-20 15:54:57 +0100

    added solution to 10.1

---
+++ chapter-10/01_sorted_merge.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+def sorted_merge(a, b):
+    bix = len(b) - 1
+    aix = len(a) - len(b) - 1
+    while aix >= 0 and bix >= 0:
+        if a[aix] > b[bix]:
+            a[aix + bix + 1] = a[aix]
+            aix -= 1
+        else:
+            a[aix + bix + 1] = b[bix]
+            bix -= 1
+    while bix >= 0:
+        a[bix] = b[bix]
+        bix -= 1
+
+
+class Test(unittest.TestCase):
+    def test_sorted_merge(self):
+        a = [1, 2, None, None]
+        b = [3, 4]
+        sorted_merge(a, b)
+        self.assertEqual(a, [1, 2, 3, 4])
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ chapter-8/04_power_set.py
@@ -0,0 +1,33 @@
+import unittest
+
+
+def find_subsets(master_set, index):
+    all_subsets = []
+    if len(master_set) == index:
+        if [] not in all_subsets:
+            all_subsets.append([])
+    else:
+        all_subsets = find_subsets(master_set, index + 1)
+        item = master_set[index]
+        more_subsets = []
+        for subset in all_subsets:
+            new_subset = []
+            [new_subset.append(value)
+                for value in subset if value not in new_subset]
+            new_subset.append(item)
+            more_subsets.append(new_subset)
+            [all_subsets.append(value)
+                for value in more_subsets if value not in new_subset]
+    return all_subsets
+
+
+class test(unittest.TestCase):
+    def test_find_subsets(self):
+        test_set = [1, 2, 3]
+        print(test_set)
+        expected = [[], [1], [2], [3], [12], [123]]
+        self.assertEqual(find_subsets(test_set, 0), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
