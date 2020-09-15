17a2c1651c8ac3c173ce344903414e467144be89
James Cockbain <james.cockbain@ibm.com>
2020-09-15 15:09:46 +0100

    add binary to string

---
+++ chapter-05/Q02_binary_to_string.py
@@ -0,0 +1,26 @@
+import unittest
+
+
+def binary_to_string(n):
+    if n >= 1 or n < 0:
+        raise Exception("Out of bounds")
+
+    binary = "0."
+    while n > 0:
+        if len(binary) >= 32:
+            raise Exception("Insufficient precision")
+
+        r = n * 2
+        if r >= 1:
+            binary += "1"
+            n = r - 1
+        else:
+            binary += "0"
+            n = r
+    return binary
+
+
+class Test(unittest.TestCase):
+    def test_binary_to_string(self):
+        self.assertEqual("0.11", binary_to_string(0.75))
+        self.assertEqual("0.101", binary_to_string(0.625))
