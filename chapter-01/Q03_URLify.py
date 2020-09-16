00edfbfc6a57843c3aadcacf30879deb248654ce
James Cockbain <james.cockbain@ibm.com>
2020-09-16 09:58:20 +0100

    fix urlify

--- chapter-01/Q03_URLify.py
+++ chapter-01/Q03_URLify.py
@@ -3,22 +3,21 @@ import unittest
 
 def urlify(string, length):
     new_index = len(string)
-
+    res = [s for s in string]
     for i in reversed(range(length)):
-        if string[i] == " ":
-            string[new_index - 3 : new_index] = "%20"
+        if res[i] == " ":
+            res[new_index - 3 : new_index] = "%20"
             new_index -= 3
         else:
-            string[new_index - 1] = string[i]
+            res[new_index - 1] = string[i]
             new_index -= 1
 
-    return string
+    return "".join(res)
 
 
 class Test(unittest.TestCase):
     def test_urlify(self):
-        # self.assertEqual("Mr%20John%20Smith", urlify("Mr John Smith    ", 13))
-        pass
+        self.assertEqual("Mr%20John%20Smith", urlify("Mr John Smith    ", 13))
 
 
 if __name__ == "__main__":
